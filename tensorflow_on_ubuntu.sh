0f7cfa1ef2e0cee1883276f8c771eb821a23f37b
MXQ <root@jojo-m.cn>
2020-08-04 17:20:06 +0800

    for Music_game_2

--- tensorflow_on_ubuntu.sh
+++ tensorflow_on_ubuntu.sh
@@ -6,4 +6,4 @@ pip install tensorflow==1.15.3 -i https://pypi.tuna.tsinghua.edu.cn/simple/ --ti
 pip install pandas -i https://pypi.tuna.tsinghua.edu.cn/simple/ --timeout=1000
 pip install matplotlib -i https://pypi.tuna.tsinghua.edu.cn/simple/ --timeout=1000
 pip install opencv-python -i https://pypi.tuna.tsinghua.edu.cn/simple/ --timeout=1000
-
+pip install librosa -i https://pypi.tuna.tsinghua.edu.cn/simple/ --timeout=1000
