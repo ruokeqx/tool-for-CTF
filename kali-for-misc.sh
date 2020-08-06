7f49cd57784582f766572345f13f22d40b1b0e7e
MXQ <root@jojo-m.cn>
2020-08-06 13:14:52 +0800

    add mimikatz

--- kali-for-misc.sh
+++ kali-for-misc.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 # 换源
-rm /etc/apt/sources.list
-echo "deb http://mirrors.tuna.tsinghua.edu.cn/kali kali-rolling main contrib non-free" > /etc/apt/sources.list
-echo "deb-src https://mirrors.tuna.tsinghua.edu.cn/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
+sudo rm /etc/apt/sources.list
+sudo echo "deb http://mirrors.tuna.tsinghua.edu.cn/kali kali-rolling main contrib non-free" > /etc/apt/sources.list
+sudo echo "deb-src https://mirrors.tuna.tsinghua.edu.cn/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
 apt-get update
-# base
-apt-get install -y python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
 # pip
-apt-get install -y python-pip
+sudo apt-get install -y python-pip
+sudo apt-get install -y python3-pip
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-pip install opencv-python --timeout=1000
-pip install matplotlib
 # mkdir
-mkdir /root/ctf
-cd /root/ctf
+mkdir ~/ctf
+cd ~/ctf
 # volatility
-apt-get install -y volatility
+sudo apt-get install -y volatility
 # foremost
-apt-get install -y foremost
+sudo apt-get install -y foremost
+# pngcheck
+sudo apt-get install -y pngcheck
 # imagemagick(convert)
-apt-get install -y imagemagick
+sudo apt-get install -y imagemagick
 # exiftool
-apt-get install -y exiftool
+sudo apt-get install -y exiftool
 # steghide
-apt-get install -y steghide
+sudo apt-get install -y steghide
 # 盲水印
 git clone https://github.com/chishaxie/BlindWaterMark
-# outguess隐写
-git clone https://github.com/crorvick/outguess
 # F5隐写
 git clone https://github.com/matthewgao/F5-steganography
 # lsb
 git clone https://github.com/livz/cloacked-pixel
+# outguess隐写
+git clone https://github.com/crorvick/outguess
+cd outguess
+sudo ./configure
+sudo make
+sudo make install
 # zsteg
 gem sources --remove https://rubygems.org/
 gem sources --add https://gems.ruby-china.com/
